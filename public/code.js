0d20ebdff99bea365e05efc740785267c9ba49d5
minmin0109 <supitsara2547@gmail.com>
2024-02-10 14:10:23 +0700

    first commit

--- public/code.js
+++ public/code.js
@@ -1,68 +1,91 @@
 (function () {
-    let receiverID;
+    let receiverID; 
     const socket = io();
 
     function generateID() {
         return `${Math.trunc(Math.random() * 999)}-${Math.trunc(Math.random() * 999)}-${Math.trunc(Math.random() * 999)}`;
     }
+
     document.querySelector("#sender-start-con-btn").addEventListener("click", function () {
-        let joinID = generateID();
+        //จะเริ่มทำงานเมื่อกด click  โดยจะสร้างไอดีและส่งข้อมูลไปยังการเชื่อมต่อ
+        let joinID = generateID(); 
+        //จะแทนที่ข้อความภายใน element ที่มี id เป็น "join-id" 
         document.querySelector('#join-id').innerHTML = `
-        <b>Room ID </b>
-        <span>${joinID}</span>
+            <b>Room ID </b>
+            <span>${joinID}</span>
         `;
+        //ส่งข้อมูลการเชื่อมต่อไปยัง server โดยใช้ Socket.IO 
         socket.emit("sender-join", {
             uid: joinID
         });
     });
-    socket.on("init", function (uid) {
+
+    socket.on("init", function (uid) { 
         receiverID = uid;
         document.querySelector(".join-screen").classList.remove("active");
         document.querySelector(".fs-screen").classList.add("active");
     });
+
     document.querySelector("#file-input").addEventListener("change", function (e) {
-        let file = e.target.files[0];
+        let file = e.target.files[0]; //เลือกไฟล์ที่ต้องการส่ง
         if (!file) {
             return;
         }
+        //อ่านไฟล์และส่งข้อมูลไปยัง server 
         let reader = new FileReader();
         reader.onload = function (e) {
             let buffer = new Uint8Array(reader.result);
+            // สร้าง Uint8Array เพื่อเก็บข้อมูลจากไฟล์ที่อ่านเข้ามา 
+            //โดยใช้ข้อมูลจาก reader.result ที่ได้อ่านได้จาก FileReader
             let el = document.createElement("div");
             el.classList.add("item");
             el.innerHTML = `
-            <div class= "progress"> 0% </div>
-            <div class= "filename"> ${file.name} </div>
-            `;
+                <div class= "progress"> 0% </div>
+                <div class= "filename"> ${file.name} </div>
+            `; //แสดง % ที่ขึ้นในหน้าจอ โดยใช้ HTML
+            
             document.querySelector(".file-list").appendChild(el);
             shareFile({
                 filename: file.name,
                 total_buffer_size: buffer.length,
                 buffer_size: 1024
-            }, buffer, el.querySelector(".progress"));
+            }, buffer, el.querySelector(".progress")); 
+            //ได้ element ที่มี class ชื่อ "progress" ซึ่งจะถูกใช้ในการอัพเดตค % ของการส่งไฟล์
         }
         reader.readAsArrayBuffer(file);
     });
 
     function shareFile(metadata, buffer, progress_node) {
-        console.log("Metadata:", metadata);
-        socket.emit("file-meta", {
+        //console.log("Metadata:", metadata);
+        socket.emit("file-meta", { //ส่งข้อมูล metadata ไปยังเซิร์ฟเวอร์
             uid: receiverID,
             metadata: metadata
         });
-        socket.on("fs-share", function () {
-            console.log("Received fs-share event");
-            let chunk = buffer.slice(0, metadata.buffer_size);
-            buffer = buffer.slice(metadata.buffer_size, buffer.length);
-            progress_node.innerText = Math.trunc((metadata.total_buffer_size - buffer.length) / metadata.total_buffer_size * 100) + "%";
-            if (chunk.length != 0) {
-                socket.emit("file-raw", {
-                    uid: receiverID,
-                    buffer: chunk
-                });
-                console.log("Sent file-raw event");
+
+        function sendChunk() {
+            if (buffer.length === 0) {
+                // ถ้าไม่มีข้อมูลใน buffer แล้วแสดงว่าไฟล์ได้ถูกส่งทั้งหมดแล้ว
+                //console.log("File transmission completed.");
+                return;
             }
-        });
+
+            let chunk = buffer.slice(0, metadata.buffer_size);
+            //ใช้ในการแบ่งข้อมูล
+            buffer = buffer.slice(metadata.buffer_size, buffer.length);
+            //ลบข้อมูลเพื่อไม่ให้ส่งซ้ำ
+            progress_node.innerText = Math.trunc((metadata.total_buffer_size - buffer.length) / metadata.total_buffer_size * 100) + "%";
+            //คำนวน %
+
+            socket.emit("file-raw", {
+                uid: receiverID,
+                buffer: chunk
+            });
+            //console.log("Sent file-raw event");
+
+            // อัพเดตโปรเกรสหลังจากส่งข้อมูล ลดเวลาในการทำงาน
+            //ให้ส่งข้อมูลชิ้นใหม่ถัดๆไปโดยไม่ต้องรอ
+            setTimeout(sendChunk, 0);
+        }
+        sendChunk();
     }
-    
 })();
--- public/receiver.js
+++ public/receiver.js
@@ -1,53 +1,87 @@
 (function () {
     let senderID;
     const socket = io();
-
+  
+    /*socket.on("connect", function () {
+      console.log("Connected to server!");
+    });*/ 
+  
     function generateID() {
-        return `${Math.trunc(Math.random() * 999)}-${Math.trunc(Math.random() * 999)}-${Math.trunc(Math.random() * 999)}`;
+      return `${Math.trunc(Math.random() * 999)}-${Math.trunc(Math.random() * 999)}-${Math.trunc(Math.random() * 999)}`;
     }
+
+    function downloadBlob(blob, filename) {
+      const url = window.URL.createObjectURL(blob); // สร้าง element สำหรับการดาวน์โหลด
+      const a = document.createElement('a'); //กำหนดคลิงค์ได้จากการสร้าง element
+     //กำหนดให้ element a ถูกซ่อน , กำหนดค่า URL ให้ a , กำหนด attribute download
+      a.style.display = 'none';
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a); 
+      //ทำให้ลิงก์ที่สร้างขึ้นเป็นส่วนหนึ่งของเนื้อหาที่แสดงบนหน้าเว็บไซต์ 
+      //และผู้ใช้จะสามารถเห็นลิงก์นี้และใช้งานได้
+
+      a.click(); // ดาวน์โหลดไฟล์
+      
+      window.URL.revokeObjectURL(url); // ลบ element
+      document.body.removeChild(a); //พื่อทำความสะอาดและป้องกันการสร้าง element เพิ่มเติมที่ไม่จำเป็นใน DOM ในภายหลัง
+    }
+  
     document.querySelector("#receiver-start-con-btn").addEventListener("click", function () {
-        senderID = document.querySelector("#join-id").value;
-        if (senderID.length == 0) {
-            return;
-        }
-        let joinID = generateID();
-        socket.emit("receiver-join", {
-            uid: joinID,
-            sender_uid: senderID
-        });
-        document.querySelector(".join-screen").classList.remove("active");
-        document.querySelector(".fs-screen").classList.add("active");
+      senderID = document.querySelector("#join-id").value;
+      if (!senderID) {
+        //console.error("senderID is not set.");
+        return;
+      }
+      //console.log("senderID: ", senderID);
+  
+      let joinID = generateID();
+      socket.emit("receiver-join", {
+        uid: joinID,
+        sender_uid: senderID
+      });
+      document.querySelector(".join-screen").classList.remove("active");
+      document.querySelector(".fs-screen").classList.add("active");
     });
+  
     let fileShare = {};
+    let totalBufferSize = 0; // เพิ่มตัวแปรเก็บขนาดของข้อมูลทั้งหมด
     socket.on("fs-meta", function (metadata) {
-        fileShare.metadata = metadata;
-        fileShare.transmitted = 0;
-        fileShare.buffer = [];
-        let el = document.createElement("div");
-        el.classList.add("item");
-        el.innerHTML = `
-            <div class= "progress"> 0% </div>
-            <div class= "filename"> ${metadata.filename} </div>
-            `;
-        document.querySelector(".file-list").appendChild(el);
+      fileShare.metadata = metadata; //เก็บข้อมูลทั้งหมด
+      fileShare.transmitted = 0; // รีเซ็ตค่า Transmitted เมื่อได้รับข้อมูล metadata ใหม่
+      fileShare.buffer = [];
+      totalBufferSize = metadata.total_buffer_size; // กำหนดค่าขนาดของข้อมูลทั้งหมด
+      let el = document.createElement("div");
+      el.classList.add("item");
+      el.innerHTML = `
+              <div class= "progress"> 0% </div>
+              <div class= "filename"> ${metadata.filename} </div>
+              `;
+      document.querySelector(".file-list").appendChild(el);
+  
+      fileShare.progress_node = el.querySelector(".progress");
+    });
 
-        fileShare.progress_node = el.querySelector(".progress");
+    socket.on("fs-share", function (buffer) {
+      //console.log("buffer: ", buffer);
+      fileShare.buffer.push(buffer);
+      //ได้รับข้อมูล buffer จากเซิร์ฟเวอร์ โค้ดจะทำการ push buffer ลงใน fileShare.buffer 
+      //ซึ่งเป็นตัวแปรที่ใช้เก็บข้อมูล buffer ที่ได้รับมาเพื่อจะนำไปใช้ในการสร้างไฟล์ในภายหลัง
 
-        socket.on("fs-share", function (buffer) {
-            console.log("Received data from sender:", buffer);
-            fileShare.buffer.push(buffer);
-            fileShare.transmitted += buffer.byteLength;
-            fileShare.progress_node.innerText = Math.trunc(fileShare.transmitted / fileShare.metadata.total_buffer_size * 100) + "%";
-            if (fileShare.transmitted == fileShare.metadata.total_buffer_size) {
-                console.log("File transmission completed.");
-                download(new Blob(fileShare.buffer), fileShare.metadata.filename);
-                fileShare = {};
-            } else {
-                console.log("Requesting more data from sender...");
-                socket.emit("fs-start", {
-                    uid: senderID
-                });
-            }
-        });        
-    })
+      fileShare.transmitted += buffer.byteLength; //เพิ่มขนาดของ buffer ที่ได้รับมาล่าสุดเข้าไป
+
+      const progress = (fileShare.transmitted / totalBufferSize) * 100; // คำนวณ %
+
+      fileShare.progress_node.innerText = Math.trunc(progress) + "%";
+      if (fileShare.transmitted == totalBufferSize) {
+        //console.log("File transmission completed.");
+        downloadBlob(new Blob(fileShare.buffer), fileShare.metadata.filename);
+        fileShare = {};
+      } else {
+        //console.log("Requesting more data from sender...");
+        socket.emit("fs-start", { // เพื่อให้เซิร์ฟเวอร์เริ่มส่งข้อมูลต่อไป
+          uid: senderID 
+        });
+      }
+    });
 })();
